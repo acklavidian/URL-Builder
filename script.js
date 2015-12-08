34a5f6a05f8aebe99a6c8f1554c36848dfb6bd65
Eddie Stanley <estanley@Yutakas-MacBook-Pro.local>
2015-12-08 12:56:39 -0600

    Added fulcrum facade and other tweaks

--- script.js
+++ script.js
@@ -1,132 +1,132 @@
-    var urlParams = document.getElementsByClassName("inputFieldValue");
-    urlParams = Array.prototype.slice.call(urlParams);
+var urlParams = document.getElementsByClassName("inputFieldValue");
+urlParams = Array.prototype.slice.call(urlParams);
+
+//Function for the disabling of Survey Number
+function disableSurveyNumCheckbox() {
+    var checkBox = document.getElementById("survey_num_checkbox_input"),
+        warnBox = document.getElementsByClassName("warn-cell"),
+        warnRow = document.getElementById("warn_row");
+        aliasInput = document.getElementById("survey_num_alias_input");
+    if (checkBox.checked) {
+        warnRow.style.display = "table-row";
+        aliasInput.disabled = true;
+    } else {
+        warnRow.style.display= "none";
+        aliasInput.disabled = false;
+    }
+}
 
 
-    //Function for the disabling of Survey Number                                   
-    function disableSurveyNumCheckbox() {
-        var checkBox = document.getElementById("survey_num_checkbox_input"),
-            warnBox = document.getElementsByClassName("warn-cell"),
-            aliasInput = document.getElementById("survey_num_alias_input");
-        if (checkBox.checked) {
-            warnBox[0].style.visibility = "visible";
-            warnBox[1].style.visibility = "visible";
-            aliasInput.disabled = true;
+
+
+//Enable and Disable Parameters
+function activateParameter(textboxName) {
+    var textbox = document.getElementById(textboxName);
+    if (textbox.disabled) {
+        textbox.disabled = false;
+    } else {
+        textbox.disabled = true;
+    }
+}
+
+
+
+
+//Update Generated URL
+function updateURL() {
+
+    var genUrlField = document.getElementById("gen_url_output"),
+        baseUrl = document.getElementById("base_url_input"),
+        urlVals = [],
+        keys = ["RID", "RISN", "SUPPLIER_ID", "AGE",
+                "GENDER", "PID", "ZIP", "MS_is_mobile",
+                "MS_is_tablet"],
+        customParam = document.getElementsByName("custom_param_row"),
+        aliasInput = "",
+        searchInput = "";
+        customParam = Array.prototype.slice.call(customParam);
+    urlParams.forEach(function (element, index, array) {
+        if (!element.disabled) {
+            urlVals.push(element.value + "=" + "[%" + keys[index] + "%]&");
+        }
+    });
+    customParam.forEach(function (element, index, array){
+        aliasInput = element.querySelectorAll('[name=aliasInput]')[0];
+        searchInput = element.querySelectorAll('[name=searchInput]')[0];
+        if (searchInput.value !== "" && aliasInput.value !== "" ){
+            urlVals.push(aliasInput.value + "=" + "[%" + searchInput.value + "%]&");
+        }
+
+        if(searchInput.value === ""){
+            searchInput.style.borderColor = "#ffbebe";
         } else {
-            warnBox[0].style.visibility = "hidden";
-            warnBox[1].style.visibility = "hidden";
-            aliasInput.disabled = false;
+            searchInput.style.borderColor = "#EEEEEE";
+
         }
-    }
-
-
-
-
-    //Enable and Disable Parameters
-    function activateParameter(textboxName) {
-        var textbox = document.getElementById(textboxName);
-        if (textbox.disabled) {
-            textbox.disabled = false;
+        if(aliasInput.value === ""){
+            aliasInput.style.borderColor = "#ffbebe";
         } else {
-            textbox.disabled = true;
+            aliasInput.style.borderColor = "#EEEEEE";
         }
+    });
+    if (baseUrl.value !== "") {
+        genUrlField.value = baseUrl.value + "/?" + urlVals.join("").slice(0, -1);
     }
 
-
-
-
-    //Update Generated URL
-    function updateURL() {
-        
-        var genUrlField = document.getElementById("gen_url_output"),
-            baseUrl = document.getElementById("base_url_input"),
-            urlVals = [],
-            keys = ["RID", "RISN", "SUPPLIER_ID", "AGE",
-                    "GENDER", "PID", "ZIP", "MS_is_mobile",
-                    "MS_is_tablet"],
-            customParam = document.getElementsByName("custom_param_row"),
-            aliasInput = "",
-            searchInput = "";
-            customParam = Array.prototype.slice.call(customParam);
-        urlParams.forEach(function (element, index, array) {
-            if (!element.disabled) {
-                urlVals.push(element.value + "=" + "[%" + keys[index] + "%]&");
-            }
-        });
-        customParam.forEach(function (element, index, array){
-            aliasInput = element.querySelectorAll('[name=aliasInput]')[0];
-            searchInput = element.querySelectorAll('[name=searchInput]')[0];
-            if (searchInput.value !== "" && aliasInput.value !== "" ){
-                urlVals.push(aliasInput.value + "=" + "[%" + searchInput.value + "%]&");
-            }
-                
-            if(searchInput.value == ""){
-                searchInput.style.borderColor = "#ffbebe";
-            } else {
-                searchInput.style.borderColor = "#EEEEEE";
-
-            }
-            if(aliasInput.value == ""){
-                aliasInput.style.borderColor = "#ffbebe";
-            } else {
-                aliasInput.style.borderColor = "#EEEEEE";
-            }
-        });
-        if (baseUrl.value !== "") {
-            genUrlField.value = baseUrl.value + "/?" + urlVals.join("").slice(0, -1);
-        }
-
-    }
+}
 
 
 
 
-    //Called when plus button is clicked
-    function addParam() {
-        var addButtonRow = document.getElementById("add_button_row"),
-            paramRow = document.createElement("tr"),
-            paramSearchCell = document.createElement("td"),
-            paramAliasCell = document.createElement("td"),
-            paramSearchInput = document.createElement("input"),
-            paramAliasInput = document.createElement("input"),
-            paramDeleteCell = document.createElement("td"),
-            paramDeleteButton = document.createElement("span");
+//Called when plus button is clicked
+function addParam() {
+    var addButtonRow = document.getElementById("add_button_row"),
+        paramRow = document.createElement("tr"),
+        paramSearchCell = document.createElement("td"),
+        paramAliasCell = document.createElement("td"),
+        paramSearchInput = document.createElement("input"),
+        paramAliasInput = document.createElement("input"),
+        paramDeleteCell = document.createElement("td"),
+        paramDeleteButton = document.createElement("span");
 
-        paramRow.setAttribute("name","custom_param_row");
-        paramRow.style.verticalAlign = "center";
-        paramSearchCell.setAttribute("name", "searchCell");
-        paramAliasCell.setAttribute("name","aliasCell");
-        paramSearchInput.type = "text";
-        paramSearchInput.setAttribute("name","searchInput");
-        
-        
-        paramAliasInput.type = "text";
-        paramAliasInput.setAttribute("name","aliasInput");
-        paramDeleteButton.className = "ion-close";
-        paramDeleteButton.style.fontSize = "25px";
-        paramDeleteButton.onclick = function () {
-            paramRow.parentElement.removeChild(paramRow);
-        };
+    paramRow.setAttribute("name","custom_param_row");
+    paramRow.style.verticalAlign = "center";
+    paramSearchCell.setAttribute("name", "searchCell");
+    paramAliasCell.setAttribute("name","aliasCell");
+    paramSearchInput.type = "text";
+    paramSearchInput.setAttribute("name","searchInput");
+    paramSearchInput.placeholder = "Search For Parameters";
 
-        //Assemble the row
-        paramSearchCell.appendChild(paramSearchInput);
-        paramAliasCell.appendChild(paramAliasInput);
-        paramDeleteCell.appendChild(paramDeleteButton);
-        paramRow.appendChild(paramSearchCell);
-        paramRow.appendChild(paramAliasCell);
-        paramRow.appendChild(paramDeleteCell);
-        addButtonRow.parentElement.insertBefore(paramRow, addButtonRow);
-        paramAliasCell.style.width = "10px";
-        $(paramSearchInput).typeahead({
-                                            hint: true,
-                                            highlight: true,
-                                            minLength: 1,
-                                            maxLength: 5,
-                                        },
-                                        {
-                                            name: 'states',
-                                            source: substringMatcher(states)
-                                        });
-    }
+
+    paramAliasInput.type = "text";
+    paramAliasInput.setAttribute("name","aliasInput");
+    paramAliasInput.placeholder="Enter Key";
+    paramDeleteButton.className = "ion-close";
+    paramDeleteButton.style.fontSize = "25px";
+    paramDeleteButton.onclick = function () {
+        paramRow.parentElement.removeChild(paramRow);
+    };
+
+    //Assemble the row
+    paramSearchCell.appendChild(paramSearchInput);
+    paramAliasCell.appendChild(paramAliasInput);
+    paramDeleteCell.appendChild(paramDeleteButton);
+    paramRow.appendChild(paramSearchCell);
+    paramRow.appendChild(paramAliasCell);
+    paramRow.appendChild(paramDeleteCell);
+    addButtonRow.parentElement.insertBefore(paramRow, addButtonRow);
+    paramAliasCell.style.width = "10px";
+    $(paramSearchInput).typeahead({
+                                        hint: true,
+                                        highlight: true,
+                                        minLength: 1,
+                                        maxLength: 5,
+                                    },
+                                    {
+                                        name: 'states',
+                                        source: substringMatcher(states)
+                                    });
+}
 
 
 
@@ -205,3 +205,54 @@ var states = ["AGE",
 "STANDARD_MOVIE_DOWNLOAD"
 ];
 
+function showBuilder(){
+    var url_wiz_div = document.getElementById("url_wiz_div");
+    disableScroll();
+    url_wiz_div.style.display = "block";
+
+}
+
+function demoComplete(){
+    alert("That concludes the demo.");
+}
+
+function notAvail(){
+    alert("Feature not availble in the demo");
+}
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
